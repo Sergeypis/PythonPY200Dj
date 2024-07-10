7c720c2b59e1cf149a30b9ff6d7c486cf3545af3
Serega <ser.pisarenko@gmail.com>
2024-07-10 11:40:41 +0300

    Feat (ООП Dj): Выполнено задание 2 Лабораторой 5

--- project/settings.py
+++ project/settings.py
@@ -46,6 +46,15 @@ AUTHENTICATION_BACKENDS = (
    'django.contrib.auth.backends.ModelBackend',
 )
 
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # Пример настроек для использования консольного
+                                                                 # бэкенда (для тестирования)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'   # Использование SMTP для отправки писем
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_HOST_USER = 'tehno-piter.reg@yandex.ru'
+EMAIL_HOST_PASSWORD = 'wkmrbidvyiabqkgx'
+EMAIL_PORT = '465'
+EMAIL_USE_TLS = True
+
 LOGIN_REDIRECT_URL = '/profile/' # Чтобы после авторизации перенаправлялось на страницу пользователя
 
 SOCIAL_AUTH_GITHUB_KEY = 'Ov23lipwxVbzMWwXDnvB'
